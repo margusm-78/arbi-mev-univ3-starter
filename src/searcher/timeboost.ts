1db2d95df70ed17dbceed3bbe977f76848fbc84c
margusm-78 <128169177+margusm-78@users.noreply.github.com>
2025-08-19 16:35:36 -0400

    Delete src directory

--- src/searcher/timeboost.ts
+++
@@ -1,8 +0,0 @@
-import { ethers } from "ethers";
-import { CONFIG } from "./config";
-
-export async function sendWithTimeboostStub(provider: ethers.JsonRpcProvider, signedTx: string, bidWei: bigint) {
-  console.log(`[Timeboost STUB] Intended bid (wei):`, bidWei.toString());
-  const resp = await provider.send("eth_sendRawTransaction", [signedTx]);
-  return resp;
-}
